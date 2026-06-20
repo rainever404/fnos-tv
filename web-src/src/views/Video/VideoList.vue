be963eddf2a250be1548762d7b0617760dd52e6a
Administrator <nilserver404@gmail.com>
2026-06-21 00:23:51 +0800

    Match official list grid density

--- web-src/src/views/Video/VideoList.vue
+++ web-src/src/views/Video/VideoList.vue
@@ -1260,16 +1260,16 @@ watch(
 }
 
 .view-card-list {
-  --poster-card-width: 170px;
+  --poster-card-width: 164px;
   display: grid;
   grid-template-columns: repeat(auto-fill, var(--poster-card-width));
   justify-content: start;
-  grid-gap: 26px 20px;
+  grid-gap: 29px 21px;
   padding: 0;
 }
 
 .favorite-content .view-card-list {
-  --poster-card-width: 170px;
+  --poster-card-width: 164px;
   grid-template-columns: repeat(auto-fill, var(--poster-card-width));
   justify-content: start;
   padding-top: 10px;
